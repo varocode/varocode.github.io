4c7b8b6f23098df8ac5aa861c9013e0e1eaae5de
varocode <119469151+varocode@users.noreply.github.com>
2022-12-06 02:03:27 -0400

    initial commit

---
+++ xk-drums/js/scripts.js
@@ -0,0 +1,24 @@
+//scroll librery init
+    AOS.init();
+
+
+// navbar init
+
+document.addEventListener('DOMContentLoaded',function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+    console.log(instances)
+});    
+
+// carousel init
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+});
+
+var options = {
+    indicator: true,
+    numVisible: 5,
+    padding: 15
+}
+
